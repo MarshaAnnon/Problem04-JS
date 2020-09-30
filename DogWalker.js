6f4543da2814835429c1e255ad657c1bdacd3ed3
marsha <marshaannon@gmail.com>
2020-09-30 10:15:48 -0400

    complete exercise

--- DogWalker.js
+++ DogWalker.js
@@ -1,8 +1,20 @@
 class DogWalker {
     numberOfWalksCalculator(dogSizes){
-        let numberOfWalks = -1;
+        let totalHours = 0;
+        let dogMap = {};
+             
+        dogSizes.forEach((item)  => {
+            if(dogMap[item] === undefined) {
+                dogMap[item] = 1
+            }else {
+                dogMap[item] = dogMap[item] + 1
+            }
+        });
 
-        return numberOfWalks;
+        for(let dogs in dogMap) {
+            totalHours += Math.ceil(dogMap[dogs]/2)
+        }
+        return totalHours
     }
 }
 
